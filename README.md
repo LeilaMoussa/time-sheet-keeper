4e8f1ed2e897472d91a26c1bcbec82863d64a8d3
LeilaMoussa <58636465+LeilaMoussa@users.noreply.github.com>
2021-03-08 17:38:22 +0100

    clean and abstract

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ Record working hours easily.
 Start a work session, pause (p), restart (r/s), and end it (e) whenever. Records are written automatically into Google Sheets.
 
 Todos:
-- [ ] Beautify code, abstract things away
-- [ ] Handle midnight signal/interrupt
-- [ ] Ending routine in case of midnight
+- [ ] Finish beautifying & cleaning code, abstract more things away
+- [ ] Finish formatting (leading 0s)
+- [ ] Create & handle midnight signal/interrupt
+- [ ] Finish ending routine in case of midnight
 - [ ] Decorator for ending routine?
 - [ ] Handle starting around midnight
 - [ ] Look for cooler/better ways to do standard things with dates & times
+- [ ] How accurate are the cumulative values? (Error propagation, rounding...)
--- time_sheet_keeper.py
+++ time_sheet_keeper.py
@@ -1,5 +1,4 @@
 from datetime import datetime as dt, timedelta as td
-# need td?
 import gspread
 import oauth2client
 import constants  # My own constant values.
@@ -10,68 +9,81 @@ consts = constants.get_constants()
 sheet_name = consts['SHEET_NAME']
 comp_per_hr = consts['PER_HOUR_WAGE']
 
-def format_fields():
-    pass
+def format_fields(today, first_start, last_end, hrs_today: int, mins_today: int) -> list:
+    formatted_day = f'{today.month}/{today.day}/{today.year}'
+    formatted_start = f'{first_start.hour}:{first_start.minute}'
+    formatted_end = f'{last_end.hour}:{last_end.minute}'
 
-def write_to_sheet():
-    pass
+    if len(str(hrs_today)) == 1: hrs_today_str = '0' + str(hrs_today)
+    if len(str(mins_today)) == 1: mins_today_str = '0' + str(mins_today)
+    # should do the same thing for start & end
+    formatted_dur = f'{hrs_today_str}:{mins_today_str}'
+    assert len(formatted_dur) == 5
+    return [formatted_day, formatted_start, formatted_end, formatted_dur]
+
+def write_to_sheet(formatted_fields: list, hrs_today: int, mins_today: int, msg: str, midnight=False):
+    sh = gc.open(sheet_name)
+    wsh = sh.get_worksheet(0)
+    all_recs = wsh.get_all_records()
+
+    # Again, might put this stuff in a function.
+
+    # If first run.
+    if len(all_recs) == 0: cumulative_work = formatted_fields[3]  # Today's duration.
+    else: cumulative_work = all_recs[-1]['Cumulative work (HH:MM)']
+    
+    cumulative_hrs, cumulative_mins = map(int, cumulative_work.split(':'))
+    cumulative_hrs += hrs_today
+    cumulative_mins += mins_today
+    cumulative_work = f'{cumulative_hrs}:{cumulative_mins}'
+    # again, append the 0 when there' one digit
+
+    cumulative_hrs += cumulative_mins/60  # Floating point number of hours.
+    cumulative_compensation = round(comp_per_hr*hrs_today, 2)
+    
+    row = formatted_fields + [msg, cumulative_work, cumulative_compensation]
+    print(row)
+    wsh.append_rows([row])
+    
+    if midnight:
+        # In case I'm doing this becaue the midnight signal went off,
+        # I need to leave the message cell empty.
+        # If it's NOT midnight, I need to check the previous message cell,
+        # and write into it as well.
+        pass
+    
+    print('Done writing.')
 
 def ending_routine(today, record: list, midnight=False):
     assert len(record) > 0 and [len(elt) == 2 for elt in record]
-    # just forget about the midnight details for now
     print(record)
-    if not midnight:  # can i do a decorator instead of this?
+    if not midnight:
         msg = input('Message\n>>> ')
-        
+
+    print('Calculating...')    
     # how does this work with after midnight?
-    sum_dur = td()
+    sum_dur = td()  # This is a timedelta object.
     for (start, end) in record:
         assert end > start
         diff = end - start
         sum_dur += diff
-        
+
+    hrs_today = (sum_dur.seconds//60)//60
+    mins_today = sum_dur.seconds//60 + 1
+
+    # One block.
     first_start = record[0][0]
     last_end = first_start + sum_dur
-    formatted_start = f'{first_start.hour}:{first_start.minute}'
-    formatted_end = f'{last_end.hour}:{last_end.minute}'
-    formatted_day = f'{today.month}/{today.day}/{today.year}'
-    hrs = (sum_dur.seconds//60)//60
-    mins = sum_dur.seconds//60
-    formatted_dur = f'{hrs}:{mins}'
-    # i need to add a leading 0 if H or M is only 1 digit
-    # also would like to round this up!
-    print("now writing...")
-    # just write into sheet now
-    sh = gc.open(sheet_name) # <------- const to be taken away
-    wsh = sh.get_worksheet(0)
-    all_recs = wsh.get_all_records()
 
-    # i should probably come back and rename total --> cumulative
-    if len(all_recs) == 0:
-        total_work = formatted_dur
-    else:
-        total_work = all_recs[-1]['Cumulative work (HH:MM)']
-        total_hrs, total_mins = map(int, total_work.split(':'))
-        hrs += total_hrs
-        mins += total_mins
-        total_work = f'{hrs}:{mins}'
-
-    print('total work', total_work)
-
-    hrs += mins/60
-    compensation = round(comp_per_hr*hrs, 2)
-    row = [formatted_day, formatted_start, formatted_end, formatted_dur, msg, total_work, compensation]
-    print('row', row)
-    wsh.append_rows([row])
+    formatted_fields = format_fields(today, first_start, last_end, hrs_today, mins_today)
     
-    if midnight:
-        # check if prev msg cell is empty
-        # if so, do the thing
-        pass
-    print('done writing! bye!')
+    print("Now writing...")
+
+    write_to_sheet(formatted_fields, hrs_today, mins_today, msg)
+    
     return
 
-def state_state(choice: str) -> str: # ?
+def state_state(choice: str) -> str: # Doable with so many edge cases?
     pass
 
 def start_session():
