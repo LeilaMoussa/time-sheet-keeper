a2b02c89ce42abed335ea011f5a9c73774126564
LeilaMoussa <58636465+LeilaMoussa@users.noreply.github.com>
2021-03-31 02:17:33 +0100

    fix bugs, update todos

--- README.md
+++ README.md
@@ -4,11 +4,12 @@ Record working hours easily.
 Start a work session, pause (p), restart (r/s), and end it (e) whenever. Records are written automatically into Google Sheets.
 
 Todos:
-- [ ] Finish beautifying & cleaning code, abstract more things away
 - [ ] Finish formatting (leading 0s)
-- [ ] Create & handle midnight signal/interrupt
-- [ ] Finish ending routine in case of midnight
+- [ ] Finish beautifying & cleaning code
+- [ ] Create & handle midnight signal/interrupt, if needed
+- [ ] Finish ending routine in case of midnight, if needed
 - [ ] Decorator for ending routine?
-- [ ] Handle starting around midnight
+- [ ] Handle starting around midnight, if needed
 - [ ] Look for cooler/better ways to do standard things with dates & times
 - [ ] How accurate are the cumulative values? (Error propagation, rounding...)
+- [ ] Test timedelta with midnight
--- time_sheet_keeper.py
+++ time_sheet_keeper.py
@@ -1,4 +1,5 @@
 from datetime import datetime as dt, timedelta as td
+from math import ceil
 import gspread
 import oauth2client
 import constants  # My own constant values.
@@ -6,40 +7,47 @@ import constants  # My own constant values.
 gc = gspread.oauth()
 
 consts = constants.get_constants()
-sheet_name = consts['SHEET_NAME']
-comp_per_hr = consts['PER_HOUR_WAGE']
+SHEET_NAME = consts['SHEET_NAME']
+COMP_PER_HR = consts['PER_HOUR_WAGE']
 
 def format_fields(today, first_start, last_end, hrs_today: int, mins_today: int) -> list:
-    formatted_day = f'{today.month}/{today.day}/{today.year}'
-    formatted_start = f'{first_start.hour}:{first_start.minute}'
-    formatted_end = f'{last_end.hour}:{last_end.minute}'
+    hour_strs = [str(elt) for elt in [first_start.hour, last_end.hour, hrs_today]]
+    min_strs = [str(elt) for elt in [first_start.minute, last_end.minute, mins_today]]
 
-    if len(str(hrs_today)) == 1: hrs_today_str = '0' + str(hrs_today)
-    if len(str(mins_today)) == 1: mins_today_str = '0' + str(mins_today)
-    # should do the same thing for start & end
-    formatted_dur = f'{hrs_today_str}:{mins_today_str}'
+    for i in range(3):
+        if len(hour_strs[i]) == 1: hour_strs[i] = '0' + hour_strs[i]
+        if len(min_strs[i]) == 1: min_strs[i] = '0' + min_strs[i]
+
+    formatted_start = f'{hour_strs[0]}:{min_strs[0]}'
+    formatted_end = f'{hour_strs[1]}:{min_strs[1]}'
+    formatted_dur = f'{hour_strs[2]}:{min_strs[2]}'
+    formatted_day = f'{today.month}/{today.day}/{today.year}'
+
     assert len(formatted_dur) == 5
     return [formatted_day, formatted_start, formatted_end, formatted_dur]
 
 def write_to_sheet(formatted_fields: list, hrs_today: int, mins_today: int, msg: str, midnight=False):
-    sh = gc.open(sheet_name)
+    sh = gc.open(SHEET_NAME)
     wsh = sh.get_worksheet(0)
     all_recs = wsh.get_all_records()
 
     # Again, might put this stuff in a function.
 
-    # If first run.
-    if len(all_recs) == 0: cumulative_work = formatted_fields[3]  # Today's duration.
-    else: cumulative_work = all_recs[-1]['Cumulative work (HH:MM)']
-    
+    if len(all_recs) == 0:
+        # If first run.
+        cumulative_work = '00:00'
+    else:
+        # Not the first time.
+        cumulative_work = all_recs[-1]['Cumulative work (HH:MM)']
+
     cumulative_hrs, cumulative_mins = map(int, cumulative_work.split(':'))
     cumulative_hrs += hrs_today
     cumulative_mins += mins_today
+    # format here!!
     cumulative_work = f'{cumulative_hrs}:{cumulative_mins}'
-    # again, append the 0 when there's one digit
-
+    
     cumulative_hrs += cumulative_mins/60  # Floating point number of hours.
-    cumulative_compensation = round(comp_per_hr*hrs_today, 2)
+    cumulative_compensation = round(COMP_PER_HR*cumulative_hrs, 2)  # Rounds up.
     
     row = formatted_fields + [msg, cumulative_work, cumulative_compensation]
     print(row)
@@ -56,24 +64,26 @@ def write_to_sheet(formatted_fields: list, hrs_today: int, mins_today: int, msg:
 
 def ending_routine(today, record: list, midnight=False):
     assert len(record) > 0 and [len(elt) == 2 for elt in record]
-    print(record)
     if not midnight:
         msg = input('Message\n>>> ')
 
     print('Calculating...')    
     # how does this work with after midnight?
+    # i think it works fine
     sum_dur = td()  # This is a timedelta object.
     for (start, end) in record:
         assert end > start
         diff = end - start
-        sum_dur += diff
+        sum_dur += diff  # Timedelta object with seconds & microseconds only.
 
-    hrs_today = (sum_dur.seconds//60)//60
+    hrs_today = sum_dur.seconds//3600
     mins_today = sum_dur.seconds//60 + 1
+    print("hrs & mins today", hrs_today, mins_today)
 
-    # One block.
     first_start = record[0][0]
+    print("first start", first_start)
     last_end = first_start + sum_dur
+    print("last end", last_end)
 
     formatted_fields = format_fields(today, first_start, last_end, hrs_today, mins_today)
     
@@ -83,19 +93,19 @@ def ending_routine(today, record: list, midnight=False):
     
     return
 
-def state_state(choice: str) -> str: # Doable with so many edge cases?
+def set_state(choice: str) -> str: # Doable with so many edge cases?
     pass
 
 def start_session():
     state = 'RUNNING'
     today = dt.today()
-    print('You started a session. Good luck!')
-    record = []  # List of tuples where each tuple is a pair of date objects.
+    print('You started a session.')
+    record = []  # List of tuples where each tuple is a pair of date objects (start, end).
     start = dt.now()
     while True:
         if state == 'RUNNING':
             choice = input('pause or end\n>>> ')
-            # might abstract all this away into a set_state() function, or not
+            # might abstract all this away into set_state(), or not
             # i'd need to make a dict to map commands to states
             # and maybe even map something (commands or states) to actions
             if choice == 'p' or choice == 'pause':
